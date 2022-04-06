7b98284005f524b6f05b7707b09f22a603ff4271
Shawn Gaul <shawn.gaul@gmail.com>
2022-04-06 11:16:25 -0700

    Add license header to new classes

--- java/src/main/java/com/azure/graph/bulk/impl/model/DocumentSerializationException.java
+++ java/src/main/java/com/azure/graph/bulk/impl/model/DocumentSerializationException.java
@@ -1,7 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.graph.bulk.impl.model;
 
 public class DocumentSerializationException extends RuntimeException {
-    
+
     public DocumentSerializationException(Throwable cause) {
         super(cause);
     }
